7e218ea7599e44e276e8516ee2d68a51ddbf8abc
raventid <juliankul@gmail.com>
2015-06-18 09:50:16 +0300

    Initial commit

---
+++ Linux shell tips and tricks.rb
@@ -0,0 +1,18 @@
+1. Find process by name, catch it's pid, show the command to execute ( add   '| sh' to the end of command to execute this)
+	ps axf | grep <process name> | grep -v grep | awk '{print "kill -9 " $1}'
+
+2. Show and update info about CPU temperature
+	watch -n 1 -d sensors
+   this one could be used with with more advanced technics
+
+3. How to really clear terminal screen?
+    printf "\033c"
+
+4. Close frozen SSH session:
+    I found a similar solution for ssh: [enter]~. First press the Enter key, then tilde, and then add a dot and your ssh session will be closed and you can continue your work in back in freed terminal.
+
+
+5. Open the remote file in the local vim session:
+    :e scp://user@host/relative/path/from/home.txt
+
+
