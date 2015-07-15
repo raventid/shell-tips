99b864f56f690589bcbb3a49595b0a4cd94d2aff
Julian Kulesh <raventid@users.noreply.github.com>
2015-07-15 22:45:50 +0300

    Update Linux shell tips and tricks.rb

--- Linux shell tips and tricks.rb
+++ Linux shell tips and tricks.rb
@@ -40,7 +40,7 @@ Make sure to include the period at the end. If you have added some files they wi
 9. Count all files in directory
    find . -type f | wc -l
    
-10. ag - great tool like grep. Repo to download - 
+10. ag - great tool like grep. Repo to download
 
 11. vim-rails plugin for vim is awesome (Emodel, Econtroller and bunch of usefull commands)
 
